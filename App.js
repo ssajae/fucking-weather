10e4ead823c507e41059c5df0efe0af693a3f0ae
ENTER ORACLE <oraclenter@gmail.com>
2018-09-29 15:59:42 +0900

    modify all

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Image, ActivityIndicator} from 'react-native';
+import Weather from './Weather';
 
 export default class App extends Component {
 
     state = {
-        isLoaded: false
+        isLoaded: true
     }
 
     render() {
-        const {isLoaded} = this.state
+        const {isLoaded} = this.state;
         return (
             <View style={styles.container}>
-                {isLoaded ? null : (
+                {isLoaded ? <Weather/> : (
                     <View style={styles.loading}>
                         <Text style={styles.loadingText}>Getting the Fucking Weather</Text>
                     </View>
--- Weather.js
+++ Weather.js
@@ -1,5 +1,64 @@
 import React, {Component} from 'react';
-import { StyleSheet, View, Text} from 'react-native';
-import { LinearGradient } from 'expo';
+import {StyleSheet, View, Text} from 'react-native';
+import {LinearGradient} from 'expo';
 
+export default class Weather extends Component {
+
+    render() {
+        return (
+            <LinearGradient
+                colors={['#00C6FB', '#005BEA']}
+                style={styles.container}
+            >
+                <View style={styles.upperView}>
+                    <Text>Icon Here!</Text>
+                    <Text style={styles.temp}>24º</Text>
+                </View>
+                <View style={styles.lowerView}>
+                    <Text style={styles.title}>Title Here</Text>
+                    <Text style={styles.subtitle}>Text Here</Text>
+                </View>
+            </LinearGradient>
+
+
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    upperView:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    temp : {
+        fontSize:38,
+        backgroundColor:'transparent',
+        color: '#FFF',
+        marginTop: 10
+    },
+    lowerView :{
+        flex:1,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-end',
+        paddingLeft: 25
+    },
+    title :{
+        fontSize:38,
+        backgroundColor:'transparent',
+        color: '#FFF',
+        marginBottom: 10,
+        fontWeight: '300'
+    },
+    subtitle:{
+        fontSize:28,
+        backgroundColor:'transparent',
+        color: '#FFF',
+        marginBottom:34
+    }
+});
 
